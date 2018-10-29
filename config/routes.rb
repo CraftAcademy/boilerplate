d9127fc715a53f1b9371bb56e686ff7485ff3715
oliverochman <oliver.ochman@hotmail.com>
2018-10-29 13:47:07 +0100

    setup routes, updated user model, added API controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :cars, action: :index
+  namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  end
 end
