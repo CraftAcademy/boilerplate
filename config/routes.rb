22dbb0ac055c5912bb9a12dc4b27c47b49ec8260
oliverochman <oliver.ochman@hotmail.com>
2018-10-29 14:19:36 +0100

    overriding devise token auth controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :cars, action: :index
   namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks], controllers: {
+      sessions: 'api/sessions',
+      registrations: 'api/registrations'
+    }
   end
 end
